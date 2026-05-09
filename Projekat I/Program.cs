7fe853b82f4b5f29155d0bf995248afef47b0230
Djurdja Popov <djurdja.popov@elfak.rs>
2026-05-09 14:01:02 +0200

    Rad servera se prekida pritiskom na Enter

--- Projekat I/Program.cs
+++ Projekat I/Program.cs
@@ -4,14 +4,15 @@
     {
         public static void Main()
         {
-            //Server smo odvojili u posebnu nit
-            //Razlog za to je da bi glavna nit ostala aktivna i bila u mogucnosti da reaguje na Enter (gasenje servera)
+            // Server smo odvojili u posebnu nit
+            // da bi glavna nit ostala aktivna i bila u mogucnosti da reaguje na Enter (gasenje servera)
             HttpServer server = new HttpServer();
             //HttpServer server = new HttpServer(5050, 10); (PORT, MAX_BROJ_NITI)!!
             Thread serverThread = new Thread(server.Start);
             serverThread.Start();
 
-            Console.ReadLine(); //Ovo nam daje efekat da se server gasi pritiskom na Enter
+            // server se gasi pritiskom na Enter
+            while (Console.ReadKey().Key != ConsoleKey.Enter) { }
 
             server.Stop();
             serverThread.Join();
